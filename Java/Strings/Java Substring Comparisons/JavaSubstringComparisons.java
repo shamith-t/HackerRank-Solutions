2c765ac6b2d8b2e9f5bc464aec7938c2f6307f6d
shamith-t <89313144+shamith-t@users.noreply.github.com>
2023-06-25 20:32:09 +0530

    Update JavaSubstringComparisons.java

--- Java/Strings/Java Substring Comparisons/JavaSubstringComparisons.java
+++ Java/Strings/Java Substring Comparisons/JavaSubstringComparisons.java
@@ -9,10 +9,9 @@ public class Solution {
         // Complete the function
         // 'smallest' must be the lexicographically smallest substring of length 'k'
         // 'largest' must be the lexicographically largest substring of length 'k'
-        int n=s.length()-k+1;
         smallest=s.substring(0,k);
         largest=s.substring(0,k);
-        for(int i=0;i<n;i++){
+        for(int i=0;i<s.length()-k+1;i++){
           smallest= (smallest.compareTo(s.substring(i,i+k)) > 0) ? s.substring(i,i+k) : smallest;
           largest= (largest.compareTo(s.substring(i,i+k)) < 0) ? s.substring(i,i+k) : largest;
         }

7785e1d8d6bcffc299bf2d020781574144b4806b
shamith-t <89313144+shamith-t@users.noreply.github.com>
2023-09-12 02:37:32 +0530

    Update DigitFrequency.c

--- C/Arrays  and Strings/Digit Frequency/DigitFrequency.c
+++ C/Arrays  and Strings/Digit Frequency/DigitFrequency.c
@@ -9,55 +9,14 @@ int main() {
     char *s;
     s=malloc(1024*sizeof(char));
     scanf("%[^\n]", s);
-    int sum[10]={0,0,0,0,0,0,0,0,0,0};
+    int sum[10]={0};
     
     int i=0;
     while(s[i]!='\0'){
-      switch(s[i]){
-        case '0':
-          sum[0]+=1;
-          i++;
-          break;
-        case '1':
-          sum[1]+=1;
-          i++;
-          break;
-        case '2':
-          sum[2]+=1;
-          i++;
-          break;
-        case '3':
-          sum[3]+=1;
-          i++;
-          break;
-        case '4':
-          sum[4]+=1;
-          i++;
-          break;
-        case '5':
-          sum[5]+=1;
-          i++;
-          break;
-        case '6':
-          sum[6]+=1;
-          i++;
-          break;
-        case '7':
-          sum[7]+=1;
-          i++;
-          break;
-        case '8':
-          sum[8]+=1;
-          i++;
-          break;
-        case '9':
-          sum[9]+=1;
-          i++;
-          break;
-        default:
-          i++;
-          break;    
+      if(s[i]>='0'&&s[i]<='9'){    
+        sum[s[i]-'0']++;
       }
+      i++;
     }
     for(int i=0;i<10;i++){
       printf("%d ",sum[i]);

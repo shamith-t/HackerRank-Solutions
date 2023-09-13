589c262341d33ba00e8c5b246fdea6a37cd747a7
shamith-t <89313144+shamith-t@users.noreply.github.com>
2023-09-14 01:31:32 +0530

    Update StudentsMarksSum.c

--- C/Functions/Students Marks Sum/StudentsMarksSum.c
+++ C/Functions/Students Marks Sum/StudentsMarksSum.c
@@ -7,18 +7,12 @@
 
 int marks_summation(int* marks, int number_of_students, char gender) {
   //Write your code here.
-  int sum=0;
-  if(gender=='b'){
-    for(int i=0;i<number_of_students;i++){
-      sum+=marks[i];
-      i+=1;
-    }
+  int sum=0,i=0;
+  if(gender=='g'){
+    i=1;
   }
-  else{
-    for (int i=1; i<number_of_students;i++) {
+  for (i; i<number_of_students;i+=2) {
     sum+=marks[i];
-    i+=1;
-    }
   }
   return sum;
 }

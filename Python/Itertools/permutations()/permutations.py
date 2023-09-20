f205097cafabc310884471822337c8032f3c16ab
shamith-t <89313144+shamith-t@users.noreply.github.com>
2023-09-20 23:27:12 +0530

    Update permutations.py

--- Python/Itertools/permutations()/permutations.py
+++ Python/Itertools/permutations()/permutations.py
@@ -3,7 +3,7 @@ from itertools import permutations
 
 S,k=map(str,input().split())
 S=''.join(sorted(S))
-combi=list(permutations(S,int(k)))
+permu=list(permutations(S,int(k)))
 
-for i in combi:
+for i in permu:
   print(''.join(i))
